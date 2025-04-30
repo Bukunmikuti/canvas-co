e1c3139129ae385e0df1f66a483d7344e3be5a40
Pelumiii1 <adesokanpelumi14@gmail.com>
2025-04-30 23:52:49 +0100

    hot fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,17 @@
 <script lang="ts" setup>
-import Navbar from "@/components/Layout/Navbar.vue";
-import Sidebar from "@/components/Layout/Sidebar.vue";
+import Navbar from '@/components/Layout/Navbar.vue';
+import Sidebar from '@/components/Layout/Sidebar.vue';
 </script>
 
 <template>
-  <div class="font-helvetica [--sidebar-width:15vw]">
-    <Navbar />
-    <img src="/displayPicture.png" alt="Display Image" class="w-screen" >
-    <div class="flex">
-      <Sidebar />
-      <div
-        class="w-full min-h-[61vh] overflow-auto px-5"
-      >
-        <slot />
-      </div>
+    <div class="font-helvetica [--sidebar-width:15vw]">
+        <Navbar />
+        <img src="/displayPicture.png" alt="Display Image" class="w-screen" />
+        <div class="flex">
+            <LayoutSidebar />
+            <div class="w-full min-h-[61vh] overflow-auto px-5">
+                <slot />
+            </div>
+        </div>
     </div>
-  </div>
 </template>
